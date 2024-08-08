880cdaa2f52c65bce9ae6ec904aa12ffdf6a9fa2
sokol <sokol@ASUS>
2024-08-08 21:22:01 +0500

    Добавьте файлы проекта.

---
+++ 1.12_04.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+
+int main()
+{
+	std::fstream up("in.txt");
+	
+	if(!(up.is_open()))
+	{
+		std::cout << "Not Open";
+	}
+
+	int rows;
+	int cols;
+	int number;
+	
+	up >> rows;
+	up >> cols;
+
+	std::cout << rows << " ";
+	std::cout << cols << std::endl;
+
+	
+
+	int** arr = new int* [rows];
+
+	for (int i = 0; i < rows; i++) 
+	{
+		arr[i] = new int[cols];
+	}
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			up >> arr[i][j];
+		}
+	}
+
+
+
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = cols-1; j >= 0; --j)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+
+	return 0;
+}
